f376ba51d9612e672f900b6039c072b23ce96af0
Jayesh <jayeshkondi@gmail.com>
2026-04-02 16:41:48 +0530

    Fix SSL for Aiven connection

--- db.py
+++ db.py
@@ -10,17 +10,18 @@ def get_db():
         time_converters = converters.conversions.copy()
         time_converters[FIELD_TYPE.TIME] = converters.convert_time
         g.db = pymysql.connect(
-            host=current_app.config['MYSQL_HOST'],
-            port=int(os.environ.get('MYSQL_PORT', 3306)),
-            user=current_app.config['MYSQL_USER'],
-            password=current_app.config['MYSQL_PASSWORD'],
-            database=current_app.config['MYSQL_DB'],
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor,
-            autocommit=False,
-            conv=time_converters,
-            ssl={'ssl': True}
-        )
+    host=current_app.config['MYSQL_HOST'],
+    port=int(os.environ.get('MYSQL_PORT', 3306)),
+    user=current_app.config['MYSQL_USER'],
+    password=current_app.config['MYSQL_PASSWORD'],
+    database=current_app.config['MYSQL_DB'],
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor,
+    autocommit=False,
+    conv=time_converters,
+    connect_timeout=30,
+    ssl_disabled=False
+)
     return g.db
 
 def close_db(e=None):
